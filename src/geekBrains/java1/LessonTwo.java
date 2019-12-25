12a7ccda4dc7db977e9c51e63a849135a5278714
arcanrun <arcan.run@gmail.com>
2019-12-26 02:27:14 +0300

    5th

--- src/geekBrains/java1/LessonTwo.java
+++ src/geekBrains/java1/LessonTwo.java
@@ -21,6 +21,7 @@ public class LessonTwo {
         two();
         tree();
         fourth();
+        fifth();
 
     }
 
@@ -88,8 +89,31 @@ public class LessonTwo {
         }
 
 
-
         System.out.println("-----------end fourth---------");
-}
+    }
+
+    public static void fifth() {
+        int [] array = { 2, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+
+        int max = 0;
+        int maxIndex = 0 ;
+
+        int min = array[0];
+        int minIndex = 0;
+
+        for(int i = 0; i < array.length; i++){
+            if (array[i] > max){
+                max = array[i];
+                maxIndex = i;
+            }
+            if (array[i] < min ) {
+                min = array[i];
+                minIndex = i;
+            }
+        }
+        System.out.println("max: " + max + " index: " + maxIndex);
+        System.out.println("min: " + min + " index: " + minIndex);
+        System.out.println("-----------end fifth---------");
+    }
 
 }
