f133a5799d4fc5dd4b79282b9bad39a903e1ad79
arcanrun <arcan.run@gmail.com>
2020-01-05 02:01:31 +0300

    2nd

--- src/geekBrains/java1/LessonThree.java
+++ src/geekBrains/java1/LessonThree.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class LessonThree {
     public static void main(String[] args) {
         guessNumber();
+        guessWord();
     }
 
     public static void guessNumber() {
@@ -12,8 +13,7 @@ public class LessonThree {
         {
             while (true) newGame:{
                 System.out.println("Guess the number in between 0 and 9");
-                int guesedNumber = (int)(Math.random() * 10);
-                System.out.println(guesedNumber);
+                int guesedNumber = (int) (Math.random() * 10);
                 int tries = 0;
                 while (true) {
 
@@ -51,6 +51,25 @@ public class LessonThree {
                     tries++;
                 }
             }
+        }
+
+    }
+
+    public static void guessWord() {
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        int index = (int) (Math.random() * words.length);
+        String guessedWord = words[index];
+        while (true){
+            System.out.println("Guess the word:");
+            Scanner scan = new Scanner(System.in);
+            String yourWord = scan.nextLine().toLowerCase();
+            if (yourWord.equals(guessedWord)){
+                System.out.println("You win");
+                break;
+            }
+            System.out.println("Wrong! Tips: ");
+            System.out.println(guessedWord.substring(0,1) + guessedWord.substring(1,2) + "############# ");
+
         }
 
     }
